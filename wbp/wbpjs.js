b63f769e23479274f7d59f8107b9f15d96109f51
Greg Devany <greg.devany@yahoo.com>
2017-04-23 23:58:45 -0500

    WBP added nonogallery2 thumbnail style to New Construction

--- wbp/wbpjs.js
+++ wbp/wbpjs.js
@@ -1,14 +1,6 @@
 $(function() {
-  $('.tnpic').on('click', function(event) {
+  $('img').on('click', function(event) {
     event.preventDefault();
     console.log('in function call')
-    let imgurl = $(this).valueof().attr('src');
-    cosole.log(imgurl);
-    OpenInNewTabWinBrowser(imgurl);
-    function OpenInNewTabWinBrowser(url) {
-      var win = window.open(url, '_blank');
-      console.log('in OpenInNewTabWinBrowser');
-      win.focus();
-    }
-  })
+    
 });
